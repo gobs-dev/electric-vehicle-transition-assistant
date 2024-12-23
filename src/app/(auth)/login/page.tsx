19bbd0e45a28f9ae5f982dd05c0834ba4b24bbc5
Muhamad Taopik Gibran <49028961+mgibrann@users.noreply.github.com>
2024-12-23 22:39:17 +0700

    implement login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,78 +1,55 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
-import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import AuthGuardLayout from "@/components/layout/AuthGuardLayout";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/contexts/auth";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const router = useRouter();
-  const { data: session, status } = useSession();
+  const { toast } = useToast();
+  const { logInWithGoogle, checkUser } = useAuth();
 
-  const searchParams = useSearchParams();
-  const error = searchParams.get("error");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (session?.user) {
-      // If user needs to complete registration, redirect to signup
-      if (!session.user?.hasRegister) {
-        router.push(
-          `/signup?email=${session.user.email}&name=${session.user.name}`
-        );
-      } else {
-        router.push("/dashboard");
-      }
-    }
-  }, [session, router]);
-
-  const handleGoogleSignIn = async () => {
+  const handleGoogleLogin = async () => {
     try {
-      setIsLoading(true);
-      const result = await signIn("google", {
-        callbackUrl: "/dashboard",
-        redirect: false,
-      });
+      await logInWithGoogle();
+      const user = await checkUser();
 
-      if (result?.error) {
-        // Handle error
-        console.error(result.error);
-      } else if (result?.url) {
-        // Successful sign in, redirect to dashboard
-        router.push("/dashboard");
+      if (user) {
+        toast({
+          title: "ログイン成功",
+        });
+        router.replace("/dashboard");
+      } else {
+        router.replace("/signup");
       }
     } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
+      console.error("Error Login with Google", error);
+      toast({
+        title: "ログインに失敗しました",
+        description:
+          "もう一度お試しください。問題が解決しない場合はサポートにお問い合わせください。",
+      });
     }
+    return null;
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="w-full max-w-sm space-y-4 p-6">
-        {error === "OAuthAccountNotLinked" && (
-          <div className="bg-red-100 text-red-600 p-4 rounded-md mb-4">
-            The email on this account is already associated with another
-            provider. Please sign in using your original provider.
-          </div>
-        )}
-
-        <button
-          onClick={handleGoogleSignIn}
-          disabled={isLoading}
-          className="w-full flex items-center justify-center gap-2 rounded-lg bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          {isLoading ? (
-            <div className="w-5 h-5 border-2 border-gray-300 border-t-blue-600 rounded-full animate-spin" />
-          ) : (
-            <div>Google</div>
-            // <svg className="h-5 w-5" viewBox="0 0 24 24">
-            //   {/* Google icon SVG path */}
-            // </svg>
-          )}
-          {isLoading ? "Signing in..." : "Sign in with Google"}
-        </button>
-      </div>
-    </div>
+    <AuthGuardLayout requireAuth={false} redirectAuthenticatedTo="/dashboard">
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="w-full max-w-sm space-y-4 p-6">
+          <Button
+            variant="outline"
+            onClick={handleGoogleLogin}
+            type="button"
+            aria-label="Login With Google"
+            className="mt-[50px] flex !h-auto items-center overflow-hidden rounded-full border border-[#2D342D] bg-white px-4 py-2 text-2xl font-bold leading-[27.84px] shadow-[0px_4px_4px_0px_#00000040]"
+          >
+            Login With Google
+          </Button>
+        </div>
+      </main>
+    </AuthGuardLayout>
   );
 }
